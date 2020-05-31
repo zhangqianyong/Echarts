cb798fca48c59f7602dd6c516459019926783135
zhangqianyong <1262194391@qq.com>
2020-05-31 20:47:41 +0800

    echarts

---
+++ map.vue
@@ -0,0 +1,138 @@
+<template>
+    <div class="box">
+        <div id="map"></div>
+    </div>
+</template>
+ 
+<script>
+ 
+import china from 'echarts/map/js/china.js'
+export default {
+    data(){
+        return {
+ 
+        }
+    },
+    mounted(){
+        this.mapFn()
+    },
+    methods:{
+        mapFn(){
+            // 基于准备好的dom，初始化echarts实例
+            var mapChart = this.$echarts.init(document.getElementById('map'));
+            mapChart.setOption({
+                backgroundColor: '#404a59', //背景颜色
+                title: {
+                    text: '全国主要城市空气质量',
+                    subtext: 'data from PM25.in',
+                    sublink: 'http://www.pm25.in',
+                    x:'center',
+                },
+                legend: { //图例组件。
+                    orient: 'vertical',
+                    y: 'bottom',
+                    x:'right',
+                    data:['pm2.5'],
+                    textStyle: {
+                        color: '#fff'
+                    }
+                },
+                //是视觉映射组件，用于进行『视觉编码』，也就是将数据映射到视觉元素（视觉通道）。
+                visualMap: {
+                    min: 0, //最小值
+                    max: 600, //最大值
+                    calculable: true, //是否显示拖拽用的手柄（手柄能拖拽调整选中范围）。
+                    inRange: {
+                        color: ['#4cb0dd', '#2d79ad', '#265da0'] //颜色
+                    },
+                    textStyle: {
+                        color: '#fff'
+                    },
+                },
+                // 提示框，鼠标移入
+                tooltip:{
+                    show:true, //鼠标移入是否触发数据
+                    trigger: 'item', //出发方式
+                    formatter:'{b}-销售数量：{c}'
+                },
+                //配置地图的数据，并且显示
+                series:[
+                    {
+                        name:'地图',
+                        type: 'map',  //地图种类
+                        map: 'china', //地图类型。
+                        data:[
+                            {name: '北京',value: Math.round(Math.random()*500)},
+                            {name: '天津',value: Math.round(Math.random()*500)},
+                            {name: '上海',value: Math.round(Math.random()*500)},
+                            {name: '重庆',value: Math.round(Math.random()*500)},
+                            {name: '河北',value: Math.round(Math.random()*500)},
+                            {name: '河南',value: Math.round(Math.random()*500)},
+                            {name: '云南',value: Math.round(Math.random()*500)},
+                            {name: '辽宁',value: Math.round(Math.random()*500)},
+                            {name: '黑龙江',value: Math.round(Math.random()*500)},
+                            {name: '湖南',value: Math.round(Math.random()*500)},
+                            {name: '安徽',value: Math.round(Math.random()*500)},
+                            {name: '山东',value: Math.round(Math.random()*500)},
+                            {name: '新疆',value: Math.round(Math.random()*500)},
+                            {name: '江苏',value: Math.round(Math.random()*500)},
+                            {name: '浙江',value: Math.round(Math.random()*500)},
+                            {name: '江西',value: Math.round(Math.random()*500)},
+                            {name: '湖北',value: Math.round(Math.random()*500)},
+                            {name: '广西',value: Math.round(Math.random()*500)},
+                            {name: '甘肃',value: Math.round(Math.random()*500)},
+                            {name: '山西',value: Math.round(Math.random()*500)},
+                            {name: '内蒙古',value: Math.round(Math.random()*500)},
+                            {name: '陕西',value: Math.round(Math.random()*500)},
+                            {name: '吉林',value: Math.round(Math.random()*500)},
+                            {name: '福建',value: Math.round(Math.random()*500)},
+                            {name: '贵州',value: Math.round(Math.random()*500)},
+                            {name: '广东',value: Math.round(Math.random()*500)},
+                            {name: '青海',value: Math.round(Math.random()*500)},
+                            {name: '西藏',value: Math.round(Math.random()*500)},
+                            {name: '四川',value: Math.round(Math.random()*500)},
+                            {name: '宁夏',value: Math.round(Math.random()*500)},
+                            {name: '海南',value: Math.round(Math.random()*500)},
+                            {name: '台湾',value: Math.round(Math.random()*500)},
+                            {name: '香港',value: Math.round(Math.random()*500)},
+                            {name: '澳门',value: Math.round(Math.random()*500)},
+                            {name: '南海诸岛',value: Math.round(Math.random()*500)}
+                        ],
+                        itemStyle: { //地图区域的多边形 图形样式。
+                            emphasis:{ //高亮状态下的样试
+                                label:{
+                                    show:true,
+                                }
+                            }
+                        },
+                        zoom:1,//放大比例
+                        label: {  //图形上的文本标签，可用于说明图形的一些数据信息
+                            show:true,
+                        },
+                    },
+                    {
+                        type:'scatter',
+                        showEffectOn: 'render',//配置什么时候显示特效
+                        coordinateSystem:'geo',//该系列使用的坐标系
+                        symbolSize:10,//标记的大小
+                        data:[
+                            {name: '宜昌', value: [111.3,30.7,130]},
+                        ],
+                        zlevel:99999
+                    },
+                ]
+            })
+        }
+    }
+}
+</script>
+ 
+<style scoped>
+.box{
+    margin-top:20px;
+}
+#map{
+    width:1200px;
+    height:720px;
+}
+</style>
---
+++ sandiantu.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div id="myChart" style="width: 800px;height:600px;"></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      let myChart = this.$echarts.init(document.getElementById("myChart"));
+      let option = {
+    xAxis: {},
+    yAxis: {},
+    series: [{
+        symbolSize: 20,
+        data: [
+            [10.0, 8.04],
+            [8.0, 6.95],
+            [13.0, 7.58],
+            [9.0, 8.81],
+            [11.0, 8.33],
+            [14.0, 9.96],
+            [6.0, 7.24],
+            [4.0, 4.26],
+            [12.0, 10.84],
+            [7.0, 4.82],
+            [5.0, 5.68]
+        ],
+        type: 'scatter'
+    }]
+};
+        // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
